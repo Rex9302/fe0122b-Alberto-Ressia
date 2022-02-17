7266727739c6d0588f5d5dbf718d1e7468aa4041
Rex9302 <ressialberto@gmail.com>
2022-02-17 13:30:24 +0100

    calcolatrice finita

--- s4/g2/asset/js/script.js
+++ s4/g2/asset/js/script.js
@@ -35,14 +35,28 @@ document.getElementById('boolean').innerHTML = Boolean(a > b);
 
 //calcolatrice
 
+var operazioneCliccata = false;
+
 function tasti(valore) {
-    document.getElementById("ops").value += valore;
+
+    if (valore == "+" || valore == "-" || valore == "*" || valore == "/") {
+        if (operazioneCliccata == false) {
+            document.getElementById("ops").value += valore;
+            operazioneCliccata = true;
+        }
+    }
+
+    if (Number.isInteger(valore) || valore == ".") {
+        document.getElementById("ops").value += valore;
+    }
 }
 
 function operazione() {
     document.getElementById("ops").value = eval(document.getElementById("ops").value);
+    operazioneCliccata = false;
 }
 
 function cancella() {
     document.getElementById("ops").value = "";
+    operazioneCliccata = false;
 }
