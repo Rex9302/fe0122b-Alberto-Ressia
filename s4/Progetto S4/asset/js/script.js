ad5a649694ae1066375012ae92c809774982dc46
Rex9302 <ressialberto@gmail.com>
2022-02-18 14:31:01 +0100

    Calcolatrice aggiornata con funzioni sperimentali

--- s4/Progetto S4/asset/js/script.js
+++ s4/Progetto S4/asset/js/script.js
@@ -3,7 +3,7 @@
 var operazioneCliccata = false;
 
 function tasti(valore) {
-
+    i = 0;
     if (valore == "+" || valore == "-" || valore == "*" || valore == "/") {
         if (operazioneCliccata == false) {
             document.getElementById("ops").value += valore;
@@ -14,14 +14,35 @@ function tasti(valore) {
     if (Number.isInteger(valore) || valore == ".") {
         document.getElementById("ops").value += valore;
     }
+
+    if (document.getElementById("ops").value.length > 18) { //Se si eccedono le dimensioni del display (18 caratteri) si avrà un errore
+        document.getElementById("ops").value = 'Error';
+    }
+
 }
 
+
+// Funzione "sperimentale" --> L'uguale dopo aver fornito il risultato se premuto di nuovo divide per 2
+var i = 0;
+
 function operazione() {
-    document.getElementById("ops").value = eval(document.getElementById("ops").value);
-    operazioneCliccata = false;
+    i++;
+    if (i <= 1) {
+        document.getElementById("ops").value = eval(document.getElementById("ops").value);
+        operazioneCliccata = false;
+    }
+    if (i >= 2) {
+
+        document.getElementById("ops").value = document.getElementById("ops").value / 2;
+    }
+
+    if (document.getElementById("ops").value.length > 18) { //Se si eccedono le dimensioni del display  (18 caratteri) si avrà un errore
+        document.getElementById("ops").value = 'Error';
+    }
 }
 
 function cancella() {
     document.getElementById("ops").value = "";
     operazioneCliccata = false;
+    i = 0;
 }
