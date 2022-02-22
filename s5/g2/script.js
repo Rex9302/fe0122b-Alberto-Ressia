6d4832d35dad23cb09cfc2ca8592ba6b1ecf5329
Rex9302 <ressialberto@gmail.com>
2022-02-22 18:56:23 +0100

    esperimento fixato

--- s5/g2/script.js
+++ s5/g2/script.js
@@ -37,28 +37,40 @@ var totale = 100;
 
 function esperimento() {
 
-    tempo = setTimeout(esperimento, 100);
+    tempo = setTimeout(esperimento, 10);
 
-    let spese = Math.floor(Math.random() * 8);
+    let spese = Math.floor(Math.random() * 10);
 
     if (totale <= 50 && totale >= 45) {
-        document.querySelector('#test').innerHTML = `<br>Hai speso ben : ${spese} dobloni! Ti rimangono ${totale} dobloni. <b>Sei al 50%!</b>`
+        document.querySelector('#test').innerHTML = `<br>Hai speso ben : ${spese} dobloni! Ti rimangono ${totale} dobloni. Sei al 50%!`
         console.log('Il totale è al 50%!')
 
     }
     if (totale <= 25 && totale >= 20) {
-        document.querySelector('#test1').innerHTML = `<br>Hai speso ben : ${spese} dobloni! Ti rimangono ${totale} dobloni. <b>Sei al 25%!</b>`
+        document.querySelector('#test1').innerHTML = `<br>Hai speso ben : ${spese} dobloni! Ti rimangono ${totale} dobloni. Sei al 25%!`
         console.log('Il totale è al 25%!')
 
     }
-    if (totale <= 0) {
-        document.querySelector('#test2').innerHTML = `<b>Hai quasi finito i dobloni!</b>`;
+    if (totale <= 10) {
+        document.querySelector('#test2').innerHTML = `Hai quasi finito i dobloni!`;
         console.log('Hai quasi finito i dobloni!');
-        clearTimeout(tempo);
-
     }
 
+
+
     totale = totale - spese;
-    document.querySelector('#lista').innerHTML += `<p> Hai speso ben : ${spese} dobloni! Ti rimangono ${totale} dobloni`
+
+    if (totale == 0){
+        document.querySelector('#test3').innerHTML = `Hai finito i dobloni!`;
+        clearTimeout(tempo);
+    }
+
+    if (totale < 0){
+        document.querySelector('#barbun').innerHTML = `Sei in debito di: ${totale} dobloni! Paga!`;
+        clearTimeout(tempo);
+    }
+
+    document.querySelector('#lista').innerHTML += `<p> Hai speso ben : ${spese} dobloni! Ti rimangono ${totale} dobloni</p>`
+
 
 }
