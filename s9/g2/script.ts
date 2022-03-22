ecb8c39faf68dbfaef013838855393447be41bb5
Rex9302 <ressialberto@gmail.com>
2022-03-22 20:06:22 +0100

    s9g2

--- s9/g2/script.ts
+++ s9/g2/script.ts
@@ -18,22 +18,24 @@ var audio = document.querySelector('#audio')
 function creaListAudio(p: any) {
     for (var i = 0; i < p.length; i++) {
         var creaAudioLi: any = document.createElement('li')
-        creaAudioLi.addEventListener('click', pAudio)
         selAudio.append(creaAudioLi)
+        creaAudioLi.addEventListener('click', pAudio)
+        creaAudioLi.setAttribute('id', p[i].audio_url)
         creaAudioLi.innerText = p[i].audio_name
     }
 }
 function creaListaVideo(p: any) {
     for (var i = 0; i < p.length; i++) {
         var creaVideoLi = document.createElement('li')
-        creaVideoLi.addEventListener('click', pVideo)
         selVideo.append(creaVideoLi)
+        creaVideoLi.addEventListener('click', pVideo)
+        creaVideoLi.setAttribute('id', p[i].video_url)
         creaVideoLi.innerText = p[i].video_name
     }
 }
 function pVideo() {
-    video.setAttribute('src', 'video/' + this.innerText + '.mp4')
+    video.setAttribute('src', 'video/' + this.id)
 }
 function pAudio() {
-    audio.setAttribute('src', 'audio/' + this.innerText + '.mp3')
+    audio.setAttribute('src', 'audio/' + this.id)
 }
